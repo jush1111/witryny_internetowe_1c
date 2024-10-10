ee0f8666f649847256f50165841a42aa03aec3fc
filipparkita1@gmail.com <filipparkita1@gmail.com>
2024-10-10 19:55:12 +0200

    nadrabiam zaleglosci

--- 10_09_2024/script.js
+++ 10_09_2024/script.js
@@ -1,2 +1,6 @@
-document.write("nazwisko: Parkita");
-console.log("WIZYTÓKA");
+console.log("Wizytowka Mikołaj Kowalski");
+document.write("<h2>Mikołaj Kowalski</h2>");
+document.write("<h2>Kontakt: 846 965 342</h2>");
+document.write("<h2>miko.kowal@gmail.com</h2>");
+document.write("<h2>Adres: ul. Nowa 21/7, Kielce</h2>");
+document.write("<hr/>");
